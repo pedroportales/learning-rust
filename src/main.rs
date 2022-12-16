ff34902502fd1a7e2076ca1248b54e2e17244889
Pedro Portales <64710575+pedroportales@users.noreply.github.com>
2022-12-16 17:20:18 +0000

    gitignore and seconds_in_hour

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+const SECONDS_IN_MINUTE: u32 = 60;
+const MINUTES_IN_HOUR: u32 = 60;
+const SECONDS_IN_HOUR: u32 = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+
 fn main() {
     let total = 30;
-    { // inicio
-        let total = "quarenta";
-        println!(" Trabalhou {} horas", total);
-    } // fim
-    println!(" Trabalhou {} horas", total);
+    let total_em_segundos = total * SECONDS_IN_HOUR;
+    println!(" Trabalhou {} segundos", total_em_segundos);
 }
